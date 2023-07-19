fa289f1f4c990e44210bee5bc0a6f8c7b31d92bf
Vitaly <kva-nvk86@mail.ru>
2023-07-19 16:34:53 +0300

    Task 4

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -4,7 +4,7 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
-
+/*
 Console.Write("Введите целое число: ");
 int a = int.Parse(Console.ReadLine()!);
 
@@ -19,6 +19,30 @@ else
 {
     max = b;
 }
+Console.Write("max = ");
+Console.WriteLine(max);
+*/
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+Console.Write("Введите целое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите целое число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите целое число: ");
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
 Console.Write("max = ");
 Console.WriteLine(max);
 
